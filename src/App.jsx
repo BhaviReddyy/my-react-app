ee86c40e4eab353bc75e45179eb1e6ae7f9bece2
bhavya <bhavyareddy506@gmail.com>
2025-12-10 23:19:35 +0530

    Updated MessageCard styling and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import Calculator from "./Calculator";
+import MessageCard from "./MessageCard";
 
 function App() {
   return (
-    <div>
-      <Calculator />
+    <div style={{ display: "flex", gap: "20px", padding: "20px" }}>
+      <MessageCard 
+        title="Welcome" 
+        message="This is the first message card!" 
+      />
+
+      <MessageCard 
+        title="Updates" 
+        message="Here are some recent updates." 
+      />
+
+      <MessageCard 
+        title="Reminder" 
+        message="Don't forget to submit your assignments!" 
+      />
+
+      <MessageCard 
+        title="Thank You!" 
+        message="Thank you for learning React!" 
+      />
     </div>
   );
 }

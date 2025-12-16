04787d5b1271d573f28ea329a9f835de99c9e6b8
bhavya <bhavyareddy506@gmail.com>
2025-12-16 14:19:05 +0530

    Added props drilling example with deep component hierarchy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import VideoPlayer from "./VideoPlayer";
+import Component1 from "./components/Component1";
 
 function App() {
   return (
     <div>
-      <VideoPlayer />
+      <h2>Props Drilling Example</h2>
+      <Component1 />
     </div>
   );
 }
 
 export default App;
+
 

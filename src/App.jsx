6dd373604edc5d0e0c11434f5f316090c87a7e30
bhavya <bhavyareddy506@gmail.com>
2025-12-17 17:39:04 +0530

    Context API to solve props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import Component1 from "./components/Component1";
 
 function App() {
-  return (
-    <div>
-      <h2>Props Drilling Example</h2>
-      <Component1 />
-    </div>
-  );
+  return <Component1 />;
 }
 
 export default App;
